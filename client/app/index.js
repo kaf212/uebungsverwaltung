0bd2a32132cbf1cbe3b086041164b624206271f3
kaf212 <kaf221122@gmail.com>
2024-04-20 22:35:19 +0200

    created functional preview

--- client/app/index.js
+++ client/app/index.js
@@ -16,3 +16,12 @@ document.getElementById("uebForm").addEventListener("submit", (event)=>{
     event.currentTarget.reset()
 })
 
+fetch("http://localhost:3000/api/uebungen").then(res => res.json()).then(res => {
+    res.forEach(item =>{
+        document.getElementById("preview").innerHTML += "<div></div>"
+        document.getElementById("preview").lastChild.innerHTML = `<div>${item.date}: <b>${item.title}</b><br></div>`
+    })
+})
+
+
+
