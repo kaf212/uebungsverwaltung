b39f75609df82ba3eb548ecffb8c4e17dcf8424b
kaf212 <kaf221122@gmail.com>
2024-04-26 11:48:57 +0200

    implemented activity archive

--- client/api/fetch-data.js
+++ client/api/fetch-data.js
@@ -1,6 +1,3 @@
-
-
-
 async function getSingleUebung(id) {
     let uebung = undefined
 
@@ -14,58 +11,65 @@ async function loadSingleUebung(id) {
     document.getElementsByClassName("main-container")[0].innerHTML += `<div><h1>${uebung.date}: <b>${uebung.title}</h1></b><br><h3>${uebung.people}</h3><br><h3>${uebung.place}</h3><br>${uebung.program}</div>`
 }
 
+async function getAllUebungen() {
+    const response = await fetch("http://localhost:3000/api/uebungen")
+    return await response.json()
+}
 
-function loadUebungenPreview() {
+
+async function loadUebungenPreview() {
     // Remove all elements from Uebungen Previews
     Array.from(document.getElementById("pastPreview").querySelectorAll("div")).forEach(ueb => ueb.remove())
     Array.from(document.getElementById("futurePreview").querySelectorAll("div")).forEach(ueb => ueb.remove())
 
-        fetch("http://localhost:3000/api/uebungen").then(res => res.json()).then(res => {
-        res.sort((a, b) => {
-            return new Date(b.date) - new Date(a.date);
-        });
-        res.forEach(item =>{
+    let res = await getAllUebungen()
+    console.log(res)
+    res = res.slice(0, 50) // limit preview to maximum of 50 Uebungen
+    res.forEach(item =>{
 
-            let leiterString = "Leitpersonen: "
-            leiterString += item.people[0]
-            for (let i=1;i<item.people.length;i++) {
-                leiterString += `, ${item.people[i]}`
-            }
-            let targetPreview = undefined
-            if (new Date(item.date) > new Date()) {
-                targetPreview = "futurePreview"
-            }
-            else {
-                targetPreview = "pastPreview"
-            }
+        let leiterString = "Leitpersonen: "
+        leiterString += item.people[0]
+        for (let i=1;i<item.people.length;i++) {
+            leiterString += `, ${item.people[i]}`
+        }
+        let targetPreview = undefined
+        if (new Date(item.date) > new Date()) {
+            targetPreview = "futurePreview"
+        }
+        else {
+            targetPreview = "pastPreview"
+        }
 
-            console.log(item)
+        console.log(item)
 
-            let imgSrc = undefined
+        let imgSrc = undefined
 
-            if (item.level === "wolf") {
-                imgSrc = "woelfe.png"
-            }
-            else {
-                imgSrc = "pfadis.png"
-            }
-            document.getElementById(targetPreview).innerHTML += `<div><div class="uebungen-preview-text-container"><p>${item.date}: ${item.title}<br>${leiterString}<br>Ort: ${item.place}</p></div> <div class="uebungen-preview-img-container"><img class="uebungen-preview-img" src="../app/img/${imgSrc}" alt=""></div></div>`
-            document.getElementById(targetPreview).lastChild.setAttribute("data-id", item._id)
+        if (item.level === "wolf") {
+            imgSrc = "woelfe.png"
+        }
+        else {
+            imgSrc = "pfadis.png"
+        }
+        document.getElementById(targetPreview).innerHTML += `<div><div class="uebungen-preview-text-container"><p>${item.date}: ${item.title}<br>${leiterString}<br>Ort: ${item.place}</p></div> <div class="uebungen-preview-img-container"><img class="uebungen-preview-img" src="../app/img/${imgSrc}" alt=""></div></div>`
+        document.getElementById(targetPreview).lastChild.setAttribute("data-id", item._id)
 
         })
+            document.getElementById("pastPreview").innerHTML += "<a href='../app/pages/archive.html'>Archiv</a>"
         addPreviewEventListeners()
-    })
+
 }
 
 function addPreviewEventListeners() {
     let allPreviewUebungElements = []
     Array.from(document.getElementsByClassName("uebungen-preview")).forEach(previewList =>{
         Array.from(previewList.children).forEach(elem =>{
-            allPreviewUebungElements.push(elem)
+            console.log(elem)
+            if (elem.tagName === "div") {
+                allPreviewUebungElements.push(elem)
+            }
 
         })
     })
-
     allPreviewUebungElements.forEach(item =>{
         item.addEventListener("click", (event)=>{
             const itemId = event.currentTarget.getAttribute('data-id')
@@ -74,4 +78,4 @@ function addPreviewEventListeners() {
     })
 }
 
-export {loadUebungenPreview, getSingleUebung, loadSingleUebung}
+export {loadUebungenPreview, getSingleUebung, loadSingleUebung, getAllUebungen, addPreviewEventListeners}
