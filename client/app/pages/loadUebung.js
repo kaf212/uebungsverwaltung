eca383604d197df95a340611c2b55e2722a9d9c5
kaf212 <kaf221122@gmail.com>
2024-08-15 17:54:04 +0200

    connected nginx to nodejs

--- client/app/pages/loadUebung.js
+++ client/app/pages/loadUebung.js
@@ -1,4 +1,4 @@
-import {loadSingleUebung} from "../api/fetch-data.js";
+import {loadSingleUebung} from "../fetch-data.js";
 
 const params = new URLSearchParams(window.location.search);
 const itemId = params.get('id');
